3aeed399ead25733f40f6182604e20fe688df08a
lightcoin <ivanlenev@gmail.com>
2013-12-29 00:08:48 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2003,7 +2003,7 @@ bool LoadBlockIndex(bool fAllowNew)
     
         
         // Genesis block
-        const char* pszTimestamp = "Building a coin to show the world how...";
+        const char* pszTimestamp = "Building the lightest coin in the world.";
         CTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
@@ -2015,13 +2015,13 @@ bool LoadBlockIndex(bool fAllowNew)
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1369623856; //epochtime
+        block.nTime    = 1388275549; //epochtime
         block.nBits    = 0x1e0ffff0;
         block.nNonce   = 1345972;
 
         if (fTestNet)
         {
-            block.nTime    = 1369591342;
+            block.nTime    = 1388275549;
             block.nNonce   = 440824;
         }
 
